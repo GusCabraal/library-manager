3142bf0b358b9fc1e782d93a14e9804831191f34
GusCabraal <gustavocabral97@hotmail.com>
2023-02-03 11:18:28 -0300

    test: route POST /books with invalid values

--- test/integration/Book.test.ts
+++ test/integration/Book.test.ts
@@ -1,15 +1,22 @@
-import supertest from 'supertest'
-import { app } from '../../src/app'
+// eslint-disable-next-line import/no-extraneous-dependencies
+import supertest from "supertest";
 
-describe('Books', () => {
-  describe('adicionando um livro no banco de dados', () => {
-    describe('quando a rota não existe', () => {
-      it('deve retornar um status 404', async () => {
+import { app } from "../../src/app";
+import { newBook } from "./mocks/books.mock";
+
+describe("Books", () => {
+  describe("adicionando um livro no banco de dados", () => {
+    describe("quando passa um parametro invalido", () => {
+      it("deve retornar um status 400 e uma mensagem de erro", async () => {
+        const { name, ...bookWithoutName } = newBook;
+
         const response = await supertest(app)
-          .post('/books')
+          .post("/books")
+          .send(bookWithoutName);
 
-        expect(response.status).toEqual(404)
-      })
-    })
-  })
-})
+        expect(response.status).toEqual(400);
+        expect(response.body).toEqual({ message: '"name" is required' });
+      });
+    });
+  });
+});
---
+++ test/integration/mocks/books.mock.ts
@@ -0,0 +1,7 @@
+export const newBook = {
+  name: "Dom Casmurro",
+  author: "Machado de Assis",
+  publishing: "Carambaia",
+  releaseYear: 1899,
+  numberOfPages: 210,
+};
