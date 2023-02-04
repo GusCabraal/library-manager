b83ff96eb664624f8673f9bebb40211e19dc439a
GusCabraal <gustavocabral97@hotmail.com>
2023-02-03 23:29:21 -0300

    test: route POST /books

--- test/integration/Book.test.ts
+++ test/integration/Book.test.ts
@@ -1,14 +1,19 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import supertest from "supertest";
+import * as sinon from 'sinon';
+import { Model } from 'sequelize';
+import Book from '../../src/database/models/Book';
 
 import { app } from "../../src/app";
-import { newBook } from "./mocks/books.mock";
+import { newBookDB, newBookInput } from "./mocks/books.mock";
 
 describe("Books", () => {
   describe("adicionando um livro no banco de dados", () => {
+    afterEach(() => sinon.restore())
+
     describe("quando passa um parametro invalido", () => {
       it("deve retornar um status 400 e uma mensagem de erro", async () => {
-        const { name, ...bookWithoutName } = newBook;
+        const { name, ...bookWithoutName } = newBookInput;
 
         const response = await supertest(app)
           .post("/books")
@@ -18,12 +23,14 @@ describe("Books", () => {
         expect(response.body).toEqual({ message: '"name" is required' });
       });
     });
+    
     describe("quando registra um livro com sucesso", () => {
       it("deve retornar um status 201 e o id do novo livro", async () => {
-        const response = await supertest(app).post("/books").send(newBook);
+        sinon.stub(Model, 'create').resolves(newBookDB as Book)
+        const response = await supertest(app).post("/books").send(newBookInput);
 
         expect(response.status).toEqual(201);
-        expect(response.body).toEqual({ id: 5 });
+        expect(response.body).toEqual(newBookDB);
       });
     });
   });
